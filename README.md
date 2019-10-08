d4dc9c20e57355a71cb5d3df48409cf94fe46521
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-08 10:59:14 -0600

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# basic-javascript-practice
-Here is where I practice DOM manipulation by JavaScript
---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Hey, I am JavaScript on your page!')
+
+let mainHeader = document.querySelector("h1")
+
+console.log(mainHeader)
+
+mainHeader.textContent = 'Thor is the best!'
+mainHeader.setAttribute
+
+let myBody = document.querySelector('body')
+
+let myDiv = document.createElement('div')
+
+myDiv.textContent = 'Hey, I am a Div!'
+
+myBody
