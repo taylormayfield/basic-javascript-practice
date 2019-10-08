3000482fe4c2c761c6c7fb21e00062ef01b2261a
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-08 11:13:14 -0600

    init commit

--- main.js
+++ main.js
@@ -5,12 +5,23 @@ let mainHeader = document.querySelector("h1")
 console.log(mainHeader)
 
 mainHeader.textContent = 'Thor is the best!'
-mainHeader.setAttribute
+mainHeader.setAttribute("style", "color:orange; border: 1px solid pink;");
 
-let myBody = document.querySelector('body')
+document.body.setAttribute("style", "background-color: #dfdfdf;")
+
+let myParagraph = document.createElement('p')
+myParagraph.textContent = 'I am the best paragraph ever written!'
 
 let myDiv = document.createElement('div')
+myDiv.appendChild(myParagraph)
 
-myDiv.textContent = 'Hey, I am a Div!'
+console.log(myDiv.textContent)
+// myDiv.textContent = 'Hey, I am a Div!'
 
-myBody
+document.body.appendChild(myDiv)
+
+let myPic = document.createElement('img')
+
+myPic.setAttribute("src", 'https://www.esquireme.com/sites/default/files/styles/full_img/public/images/2019/07/17/thor.jpg?itok=tnod8VI8')
+
+document.body.appendChild(myPic)
