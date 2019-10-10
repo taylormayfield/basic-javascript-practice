c71abf64d1e71d409c6b25bd15aff38b1e776a88
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-10 11:11:26 -0600

    adding stuff

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-import { films } from '/assests/films.js'
+import { films } from '/films.js'
+import { people } from '/people.js'
 
 console.log('Hey, I am JavaScript on your page!')
 
-console.log(films[0].opening_crawl)
-
-let film1 = document.createElement('div')
-let title1 = document.createElement('h1')
-let crawl1 = document.createElement('p')
-
-film1.appendChild(title1)
-film1.appendChild(crawl1)
-
-title1.textContent = films[0].title
-crawl1.textContent = film[0].opening_crawl
-
 let mainArea = document.querySelector('main')
-mainArea.appendChild(film1)
+
+films.forEach(function (film) {
+    let filmDiv = document.createElement('div')
+    let title = document.createElement('h1')
+    let crawl = document.createElement('p')
+
+    filmDiv.appendChild(title)
+    filmDiv.appendChild(crawl)
+
+    title.textContent = films.title
+    crawl.textContent = film.opening_crawl
+
+    mainArea.appendChild(filmDiv)
+})
+
+people.forEach(function (person) {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = documetn.createElement('h3')
+    
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+
+    name.textContent = person.name
+    gender.textContent = gender.name
+
+    mainArea.appendChild(personDiv)
+})
