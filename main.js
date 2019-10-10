ad755a4cda030de2d299f9f61d67b1553506feb1
Taylor Mayfield <taylormayfield@taylors-air-2.ad.uvu.edu>
2019-10-10 10:27:45 -0600

    init commit

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
+import { films } from '/assests/films.js'
+
 console.log('Hey, I am JavaScript on your page!')
 
-let mainHeader = document.querySelector("h1")
+console.log(films[0].opening_crawl)
 
-console.log(mainHeader)
+let film1 = document.createElement('div')
+let title1 = document.createElement('h1')
+let crawl1 = document.createElement('p')
 
-mainHeader.textContent = 'Thor is the best!'
-mainHeader.setAttribute("style", "color:orange; border: 1px solid pink;");
+film1.appendChild(title1)
+film1.appendChild(crawl1)
 
-document.body.setAttribute("style", "background-color: #dfdfdf;")
+title1.textContent = films[0].title
+crawl1.textContent = film[0].opening_crawl
 
-let myParagraph = document.createElement('p')
-myParagraph.textContent = 'I am the best paragraph ever written!'
-
-let myDiv = document.createElement('div')
-myDiv.appendChild(myParagraph)
-
-console.log(myDiv.textContent)
-// myDiv.textContent = 'Hey, I am a Div!'
-
-document.body.appendChild(myDiv)
-
-let myPic = document.createElement('img')
-
-myPic.setAttribute("src", 'https://www.esquireme.com/sites/default/files/styles/full_img/public/images/2019/07/17/thor.jpg?itok=tnod8VI8')
-
-document.body.appendChild(myPic)
+let mainArea = document.querySelector('main')
+mainArea.appendChild(film1)
