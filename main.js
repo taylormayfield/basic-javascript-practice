37f2cb245314e1aec2d5617d8443134c2b74e86f
Taylor Mayfield <taylormayfield@Taylors-MacBook-Air-2.local>
2019-10-16 13:01:18 -0600

    stuff

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { films } from '/assets/films.js'
-import { people } from '/assets/people.js'
-import { starships } from '/assets/starships.js'
-import { species } from '/assets/species.js'
-import { planets } from '/assets/planets.js'
-import { vehicles } from '/assets/vehicles.js'
+import { films } from './assets/films.js'
+import { people } from './assets/people.js'
+import { starships } from './assets/starships.js'
+import { species } from './assets/species.js'
+import { planets } from './assets/planets.js'
+import { vehicles } from './assets/vehicles.js'
 
 console.log('Hey, I am JavaScript on your page!')
 
@@ -11,14 +11,14 @@ let mainArea = document.querySelector('main')
 
 films.forEach(function(film) {
     let filmDiv = document.createElement('div')
-    let title = document.createElement('h1')
-    let crawl = document.createElement('p')
+    let filmTitle = document.createElement('h1')
+    let filmCrawl = document.createElement('p')
 
-    filmDiv.appendChild(title)
-    filmDiv.appendChild(crawl)
+    filmDiv.appendChild(filmTitle)
+    filmDiv.appendChild(filmCrawl)
 
-    title.textContent = films.title
-    crawl.innerText = film.opening_crawl
+    filmTitle.textContent = films.title
+    filmCrawl.innerText = film.opening_crawl
 
     mainArea.appendChild(filmDiv)
 })
@@ -26,16 +26,18 @@ films.forEach(function(film) {
 people.forEach(function(person) {
     let personDiv = document.createElement('div')
     let name = document.createElement('h1')
-    let gender = documetn.createElement('h3')
+    let gender = document.createElement('h3')
     let pic = document.createElement('img')
     
     personDiv.appendChild(name)
     personDiv.appendChild(gender)
     personDiv.appendChild(pic)
 
+    let charNum = getCharNumber(person.url)
+
     name.textContent = person.name
     gender.textContent = gender.name
-    pic.src = 'https://starwars-visualguide.com/assets/img/characters/$(charNum).jpg'
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
 
     mainArea.appendChild(personDiv)
 })
@@ -45,8 +47,10 @@ function getCharNumber(charURL) {
     let charID = charURL.substring(end -2, end)
     if(charID.indexOf('/') !== -1 ) {
         return charID.slice(1,2)
+    } else {
+        return charID
     }
-}
+    }
 
 const maleCharacters = people.filter(person => person.gender === 'male')
 console.log(maleCharacters)
